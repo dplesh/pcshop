00df929199997927c433fa60a041638fb3629a98
dplesh <dplesh255@gmail.com>
2016-12-02 13:13:06 +0200

    Create Controllers Base

---
+++ public/controllers/controllers.js
@@ -0,0 +1,6 @@
+/**
+ * Created by User on 02/12/2016.
+ */
+'use strict';
+
+var dmpcControllers  = angular.module('dmpcControllers', []);
---
+++ public/controllers/homeController.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var homeController = angular.module('dmpcControllers')
+    .controller('homeController', ['$scope', function($scope){
+        //TBD
+    }
+    ]);
