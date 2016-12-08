34c2691862ccb288056edc81a5d45ef736d9c808
dplesh <dplesh255@gmail.com>
2016-12-08 15:23:35 +0200

    Create Contact Service

---
+++ public/services/contactService.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var contactModule = angular.module('contactModule', []);
+
+var contactService = contactModule
+    .service('contactService', function(){
+        
+        this.submitDetails = function(name, email, phoneNumber, description){
+            
+        };
+
+    });
